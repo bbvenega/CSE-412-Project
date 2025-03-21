05774c5f85a4e93d162eaf320c82df6423df376d
Hai Tang <131619622+hiitshai@users.noreply.github.com>
2025-03-21 16:45:24 -0700

    Changing "user", "admin" to "Users" and "Admins"

--- Table Modifications/add_relationships.sql
+++ Table Modifications/add_relationships.sql
@@ -1,6 +1,6 @@
 ---------------------- Set All Primary Keys ----------------------
 -- For table "user"
-ALTER TABLE "user" ADD PRIMARY KEY (U_USER_ID);
+ALTER TABLE "Users" ADD PRIMARY KEY (U_USER_ID);
 
 -- For table WATCHLIST
 ALTER TABLE WATCHLIST ADD PRIMARY KEY (W_WATCHLIST_ID);
@@ -15,19 +15,19 @@ ALTER TABLE WATCHLIST_STOCKS ADD PRIMARY KEY (WS_WATCHLIST_ID, WS_STOCK_ID);
 ALTER TABLE HISTORICAL_STOCK ADD PRIMARY KEY (HS_RECORD_ID);
 
 -- For table admin
-ALTER TABLE "admin" ADD PRIMARY KEY (A_ADMIN_ID);
+ALTER TABLE "Admins" ADD PRIMARY KEY (A_ADMIN_ID);
 
 COMMIT WORK;
 ---------------------------------------------------------------
 
 ----------------------- Admin IS A User -----------------------
-ALTER TABLE "admin" ADD FOREIGN KEY (A_ADMIN_ID) references "user" (U_USER_ID);
+ALTER TABLE "Admins" ADD FOREIGN KEY (A_ADMIN_ID) references "Users" (U_USER_ID);
 
 COMMIT WORK;
 ---------------------------------------------------------------
 
 --------- User -> Watchlist Relationship (One to Many) ---------
-ALTER TABLE WATCHLIST ADD FOREIGN KEY (W_USER_ID) references "user" (U_USER_ID);
+ALTER TABLE WATCHLIST ADD FOREIGN KEY (W_USER_ID) references "Users" (U_USER_ID);
 
 COMMIT WORK;
 ----------------------------------------------------------------
